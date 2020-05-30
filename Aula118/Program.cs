0a560a832a8d73d982eb8df7a6b961f26a3d1ef3
Jader Vasque <jader.vasque@gmail.com>
2020-05-30 00:20:21 -0300

    Modificações diversas

--- Aula118/Program.cs
+++ Aula118/Program.cs
@@ -11,59 +11,8 @@ namespace Aula118
     {
         static void Main(string[] args)
         {
-            var ListaPessoas = new System.Collections.Generic.List<Pessoa>();
-            string entrada;
-            while (!string.IsNullOrEmpty(entrada = Console.ReadLine()))
-            {
-                string[] vet;
-                if ((vet = entrada.Split(' ')).Length == 3)
-                {
-                    var p = new Pessoa();
-                    p.Nome = vet[0];
-                    p.DataNascimento = DateTime.Parse(vet[1]);
-                    p.Naturalidade = (Cidades)Enum.Parse(typeof(Cidades), vet[2]);
-                    ListaPessoas.Add(p);
-                }
-                else
-                    Console.WriteLine("seu jumento 1");
-            }
-            string nomeBuscado;
-            while (!string.IsNullOrEmpty(nomeBuscado = PerguntarNome()))
-            {
-                if (ListaPessoas.Exists(t => t.Nome == nomeBuscado))
-                {
-                    var p = ListaPessoas.Find(t => t.Nome == nomeBuscado);
-                    var texto = new StringBuilder();
-                    texto.AppendLine($"Nome: {p.Nome}");
-                    texto.AppendLine($"Idade: {p.Idade}");
-                    texto.AppendLine($"Naturalidade: {p.Naturalidade.ToString()}");
-                    texto.AppendLine();
-                    Console.WriteLine(texto.ToString());
-                }
-                else
-                    Console.WriteLine("seu jumento 2!!!!!!!");
-            }
 
-            var mae = new Pessoa()
-            {
-                Nome = "Maria",
-                DataNascimento = DateTime.Parse("02/10/1955"),
-                Naturalidade = (Cidades)Enum.Parse(typeof(Cidades), "Rubiataba"),
-                Mae = null
-            };
 
-            var pjader = ListaPessoas.Find(t => t.Nome == mae.Nome);
-            pjader.Mae = mae;
-
-            Console.WriteLine(pjader.Mae.Nome);
-            Console.ReadLine();
-        }
-
-        static string PerguntarNome()
-        {
-            string pergunta = "Digite o nome da pessoa desejada (em branco finaliza):";
-            Console.WriteLine(pergunta);
-            return Console.ReadLine();
         }
     }
 }
