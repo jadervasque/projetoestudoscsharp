fba2176d4656d3dcd7ffa23df93f9ceb7b74d867
Jader Vasque <jader.vasque@gmail.com>
2020-06-04 18:20:55 -0300

    wip

---
+++ ExerciciosCursoCSharp_Aula118/Entities/HourContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula118.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date.ToShortDateString()}, ValuePerHour: {ValuePerHour.ToString("F2")}, Hours: {Hours}";
+        }
+    }
+
+
+}
---
+++ ExerciciosCursoCSharp_Aula118/Entities/Worker.cs
@@ -0,0 +1,47 @@
+﻿using Aula118.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Aula118.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public WorkerLevel Level { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; }
+
+        public Worker()
+        {
+            Contracts = new List<HourContract>();
+        }
+
+        public Worker(string name, double baseSalary, WorkerLevel level, Department department)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+            Level = level;
+            Department = department;
+            Contracts = new List<HourContract>();
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            var income = BaseSalary;
+            foreach (HourContract contract in Contracts)
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                    income += contract.TotalValue();
+            return income;
+        }
+    }
+}
