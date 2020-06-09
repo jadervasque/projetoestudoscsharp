37a0a0b6cede5c49f9f582b7db5014d4c460e503
Jader Vasque <jader.vasque@gmail.com>
2020-06-09 01:10:42 -0300

    Finalização Curso CSS / Início Curso JavaScript

---
+++ CursoJavaScript/fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+// sentença de código
+console.log("Sentença de código")
+
+// bloco de código / agrupador delimitado por { }
+// papel do bloco é agrupar sentenças de códigos
+{
+    {
+        console.log("Olá"); // com ponto-e-vírgula
+        console.log('Mundo!!!!') // padrão do curso
+    }
+}
---
+++ CursoJavaScript/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,23 @@
+
+var a = 3
+
+// forma mais moderna de declarar variáveis
+let b = 4
+
+// var pode ser redeclarada
+var a =30
+
+// let não pode ser redeclarada
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+console.log(a, b)
+
+// declaração de constantes
+// não pode receber novo valor
+const c = 5;
+// tudo aquilo que não tem motivo para mudar deve ser constante
+// facilita na manutenção, não sei por que ainda, f**a-se
