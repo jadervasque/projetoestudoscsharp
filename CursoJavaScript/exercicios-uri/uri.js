8ec90799cc575775af40aaf7f257fe30ceaced31
Jáder Vasque <jader.vasque@gmail.com>
2020-07-13 15:44:35 -0300

    wip

--- CursoJavaScript/exercicios-uri/uri.js
+++ CursoJavaScript/exercicios-uri/uri.js
@@ -12,28 +12,7 @@ const info = lines.shift().split(' ')
 const a = parseInt(info[0])
 const b = parseInt(info[1])
 
-let quociente, resto;
-if (a >= 0) {
-    quociente = a / b;
-    resto = a % b;
-} else {
-    let x = 0, y = 0;
 
-    if (b < 0) {
-        x = b * -1
-    }
-    else {
-        x = b
-    }
-
-    for (resto = 0; resto < x; resto++) {
-        y = a - resto;
-        if (y % b == 0) {
-            break
-        }
-    }
-    quociente = y / b;
-}
 
 // let q = Math.trunc(a / b)
 // let r = a - b * q
